e0f3afd57c653747e6639680afdbde5bb23d1577
HARI VARDANACHARI KORRAPATI <36504878+chari1239@users.noreply.github.com>
2022-07-30 00:41:29 +0530

    Update LeetCode-350-intersection-of-two-arrays-ii.cpp

--- LeetCode-350-intersection-of-two-arrays-ii.cpp
+++ LeetCode-350-intersection-of-two-arrays-ii.cpp
@@ -31,7 +31,9 @@ Constraints:
 
 */
 
-/*********** Solution ***************/
+*********** Solution 1 ***************
+
+/* This Solution is based on using HashMap */
 
 class Solution {
 public:
@@ -54,4 +56,47 @@ public:
         
     }
 };
+
+
+//   Time Complexity : O(n+m) (sum of array sizes - as we go loop through both arrays once)
+//   Space Complexity : O(n) ( it can be O(min(n,m)) if we try to compare & store the minimum elements)
+
+
+
+********* Soultion 2  ***********
+
+/* This can be a variation of above if the given arrays are sorted or you can think of a solution based on Sorting. */
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        
+       vector<int> res;
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        int n = nums1.size(), m = nums2.size();
+        
+        int i=0,j=0;
+        
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums1[i]);
+                ++i; ++j;
+            }else if(nums1[i]>nums2[j]){
+                ++j;
+            }else{
+                ++i;
+            }
+        }
+        
+        return res;
+        
+    }
+};
+
+
+// Time Complexity : O(nlogn) + O(mlogm) + O(min(n,m))
+// Space Complexity : O(1) -- no extra space
 
